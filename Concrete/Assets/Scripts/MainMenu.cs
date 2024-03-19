ed6062eb0fad3d32d8cdfa994a0ed924089ae47a
Bosspug <eugeniojlopezt@gmail.com>
2024-03-18 23:00:57 -0400

    Fade transition for main menu/level touchups
    work todo:
    UI dialogue
    make the rest of the game xdd

--- Concrete/Assets/Scripts/MainMenu.cs
+++ Concrete/Assets/Scripts/MainMenu.cs
@@ -5,14 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+    [SerializeField] public float transitionTime = 1f;
     public void PlayGame() 
-    { 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
     public void QuitGame()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    IEnumerator LoadLevel(int levelIndex) 
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
