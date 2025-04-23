c84db5ea1124a651f302cd8541a787acb0bde7e8
Bosspug <123424992+Bosspug15@users.noreply.github.com>
2025-04-22 23:34:55 -0400

    Update ConcreteMouseLook.cs

--- Concrete/Assets/Scripts/ConcreteMouseLook.cs
+++ Concrete/Assets/Scripts/ConcreteMouseLook.cs
@@ -6,19 +6,16 @@ public class ConcreteMouseLook : MonoBehaviour
 {
 
     [SerializeField] public float mouseSensitivity = 100f;
-
+    
     public Transform playerBody;
 
     float xRotation = 0f;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
